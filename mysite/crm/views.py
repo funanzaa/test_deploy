8efc11aec7d60e296eeb83783cf5379884b6b840
funanzaa <eakachai_golf@hotmail.com>
2020-10-01 11:42:37 +0700

    commit add limit show hospital pages

--- mysite/crm/views.py
+++ mysite/crm/views.py
@@ -9,7 +9,8 @@ from django.core.files.storage import FileSystemStorage
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger #paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger  # paginator
+
 
 @login_required(login_url='login')
 def dashboardPage(request):
@@ -19,19 +20,30 @@ def dashboardPage(request):
     count_hospitals_all = Hospitals.objects.all().count()
     count_project_all = Project.objects.all().count()
     count_case_all = Case.objects.all().count()
-    count_case_hos = Case.objects.filter(date_entered__month=str(now)[6:7],project_id=1,date_entered__year=str(now)[:4]).count()
-    count_project_opbkk = Case.objects.filter(date_entered__month=str(now)[6:7],project_id=2,date_entered__year=str(now)[:4]).count()
-    count_project_erefer = Case.objects.filter(date_entered__month=str(now)[6:7],project_id=3,date_entered__year=str(now)[:4]).count()
-    count_project_ehhc = Case.objects.filter(date_entered__month=str(now)[6:7],project_id=4,date_entered__year=str(now)[:4]).count()
-    count_project_hshv = Case.objects.filter(date_entered__month=str(now)[6:7],project_id=5,date_entered__year=str(now)[:4]).count()
-    count_project_smartcard = Case.objects.filter(date_entered__month=str(now)[6:7],project_id=6,date_entered__year=str(now)[:4]).count()
-    count_server = Case.objects.filter(date_entered__month=str(now)[6:7],project_id=7,date_entered__year=str(now)[:4]).count()
-    count_other = Case.objects.filter(date_entered__month=str(now)[6:7],project_id=8,date_entered__year=str(now)[:4]).count()
+    count_case_hos = Case.objects.filter(date_entered__month=str(
+        now)[6:7], project_id=1, date_entered__year=str(now)[:4]).count()
+    count_project_opbkk = Case.objects.filter(date_entered__month=str(
+        now)[6:7], project_id=2, date_entered__year=str(now)[:4]).count()
+    count_project_erefer = Case.objects.filter(date_entered__month=str(
+        now)[6:7], project_id=3, date_entered__year=str(now)[:4]).count()
+    count_project_ehhc = Case.objects.filter(date_entered__month=str(
+        now)[6:7], project_id=4, date_entered__year=str(now)[:4]).count()
+    count_project_hshv = Case.objects.filter(date_entered__month=str(
+        now)[6:7], project_id=5, date_entered__year=str(now)[:4]).count()
+    count_project_smartcard = Case.objects.filter(date_entered__month=str(
+        now)[6:7], project_id=6, date_entered__year=str(now)[:4]).count()
+    count_server = Case.objects.filter(date_entered__month=str(
+        now)[6:7], project_id=7, date_entered__year=str(now)[:4]).count()
+    count_other = Case.objects.filter(date_entered__month=str(
+        now)[6:7], project_id=8, date_entered__year=str(now)[:4]).count()
     case = Case.objects.filter(created_by=current_user).order_by('-id')[:10]
-    context = {"dates": now ,"all_case": case,"count_hospitals_all":count_hospitals_all,"count_project_all":count_project_all,"count_case_all":count_case_all}
-    return render(request, 'cases/dashboard.html',context )
+    context = {"dates": now, "all_case": case, "count_hospitals_all": count_hospitals_all,
+               "count_project_all": count_project_all, "count_case_all": count_case_all}
+    return render(request, 'cases/dashboard.html', context)
 
 # Add Case
+
+
 @login_required(login_url='login')
 def createCase(request):
     project = Project.objects.all()
@@ -85,17 +97,21 @@ def createCase(request):
             newCase.save()
             messages.success(request, 'Your case is added successfully!')
             return HttpResponseRedirect(reverse_lazy('viewcase'))
-    context = {"projects":project,"subgroups":subgroup,"services":service,"hospitals":hospital}
-    return render(request, 'cases/add_case.html',context)
+    context = {"projects": project, "subgroups": subgroup,
+               "services": service, "hospitals": hospital}
+    return render(request, 'cases/add_case.html', context)
 
-#detailCase
+# detailCase
+
 
 def detailCase(request, pk):
     case = Case.objects.get(id=pk)
-    context = { 'case': case }
+    context = {'case': case}
     return render(request, 'cases/detail_case.html', context)
 
 # update Case
+
+
 def updateCase(request, pk):
     case = Case.objects.get(id=pk)
     project = Project.objects.all()
@@ -150,8 +166,10 @@ def updateCase(request, pk):
             messages.success(request, 'Your case is updated successfully!')
             return HttpResponseRedirect(reverse_lazy('viewcase'))
             # return redirect('viewcase')
-    context = { 'case': case,"projects":project,"subgroups":subgroup,"services":service,"hospitals":hospital }
+    context = {'case': case, "projects": project, "subgroups": subgroup,
+               "services": service, "hospitals": hospital}
     return render(request, 'cases/update_case.html', context)
+
 
 @login_required(login_url='login')
 def deleteCase(request, pk):
@@ -163,12 +181,13 @@ def deleteCase(request, pk):
     context = {'case': case}
     return render(request, 'cases/delete.html', context)
 
+
 @login_required(login_url='login')
 def hospital(request):
     hospital_list = Hospitals.objects.all().order_by('code')
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
 
-    paginator = Paginator(hospital_list,10)
+    paginator = Paginator(hospital_list, 30)
     try:
         hospital = paginator.page(page)
     except PageNotAnInteger:
@@ -176,13 +195,15 @@ def hospital(request):
     except EmptyPage:
         hospital = paginator.page(paginator.num_pages)
     if request.method == 'GET':
-            search_query = request.GET.get('text_find', None)
-            if search_query:
-                hospital = Hospitals.objects.filter(label__icontains=search_query) | Hospitals.objects.filter(code__icontains=search_query)
-                context = {'hospital': hospital}
-                return render(request, 'cases/hospital.html', context)
+        search_query = request.GET.get('text_find', None)
+        if search_query:
+            hospital = Hospitals.objects.filter(
+                label__icontains=search_query) | Hospitals.objects.filter(code__icontains=search_query)
+            context = {'hospital': hospital}
+            return render(request, 'cases/hospital.html', context)
     context = {'hospital': hospital}
     return render(request, 'cases/hospital.html', context)
+
 
 @login_required(login_url='login')
 def hospitalAdd(request):
@@ -190,35 +211,36 @@ def hospitalAdd(request):
     if request.method == 'POST':
         form = hospitalForm(request.POST)
         if form.is_valid():
-                code = request.POST['code']
-                label = request.POST['label']
-                h_type = request.POST['h_type']
-                hcode = Hospitals.objects.filter(code=code)
-                Label = Hospitals.objects.filter(label=label)
-                if hcode:
-                    messages.info(request, 'This ' + code +' already')
-                elif Label:
-                    messages.info(request, 'This ' + label +' already')
-                else:
-                    newHospital = Hospitals()
-                    newHospital.code = code
-                    newHospital.label = label
-                    newHospital.h_type = h_type
-                    tz = pytz.timezone('Asia/Bangkok')
-                    newHospital.date_created = datetime.datetime.now(tz=tz)
-                    newHospital.save()
-                    messages.success(request, 'Your hospital is added successfully!')
-                    return HttpResponseRedirect(reverse_lazy('hospital-page'))
+            code = request.POST['code']
+            label = request.POST['label']
+            h_type = request.POST['h_type']
+            hcode = Hospitals.objects.filter(code=code)
+            Label = Hospitals.objects.filter(label=label)
+            if hcode:
+                messages.info(request, 'This ' + code + ' already')
+            elif Label:
+                messages.info(request, 'This ' + label + ' already')
+            else:
+                newHospital = Hospitals()
+                newHospital.code = code
+                newHospital.label = label
+                newHospital.h_type = h_type
+                tz = pytz.timezone('Asia/Bangkok')
+                newHospital.date_created = datetime.datetime.now(tz=tz)
+                newHospital.save()
+                messages.success(request, 'Your hospital is added successfully!')
+                return HttpResponseRedirect(reverse_lazy('hospital-page'))
     context = {'form': form}
     return render(request, 'cases/hospital_form.html', context)
+
 
 @login_required(login_url='login')
 def hospitalEdit(request, pk):
     edithosptial = Hospitals.objects.get(id=pk)
     form = editHospitalForm()
-    form.fields['code'].initial  = edithosptial.code
-    form.fields['label'].initial  = edithosptial.label
-    form.fields['h_type'].initial  = edithosptial.h_type
+    form.fields['code'].initial = edithosptial.code
+    form.fields['label'].initial = edithosptial.label
+    form.fields['h_type'].initial = edithosptial.h_type
     if request.method == 'POST':
         form = editHospitalForm(request.POST)
         if form.is_valid():
@@ -233,8 +255,9 @@ def hospitalEdit(request, pk):
             edithosptial.save()
             messages.success(request, 'Your hospital is updated successfully!')
             return HttpResponseRedirect(reverse_lazy('hospital-page'))
-    context = { 'form': form }
+    context = {'form': form}
     return render(request, 'cases/edit_hospital.html', context)
+
 
 @login_required(login_url='login')
 def create_case_hospital(request, pk):
@@ -249,7 +272,7 @@ def create_case_hospital(request, pk):
             project = request.POST['project']
             project_subgroup = request.POST['project_subgroup']
             resolution = request.POST['resolution']
-            service  = request.POST['service']
+            service = request.POST['service']
             upload_file = request.FILES['case_image']
             newCase = Case()
             newCase.name = case_name
@@ -272,7 +295,7 @@ def create_case_hospital(request, pk):
             project = request.POST['project']
             project_subgroup = request.POST['project_subgroup']
             resolution = request.POST['resolution']
-            service  = request.POST['service']
+            service = request.POST['service']
             newCase = Case()
             newCase.name = case_name
             newCase.project_id = project
@@ -286,15 +309,18 @@ def create_case_hospital(request, pk):
             messages.success(request, 'Your case is added successfully!')
             return HttpResponseRedirect(reverse_lazy('viewcase'))
 
-    context = {'projects': project,'subgroups':subgroup,'services': service,'hosptial': hosptial}
+    context = {'projects': project, 'subgroups': subgroup,
+               'services': service, 'hosptial': hosptial}
     return render(request, 'cases/hospital_addcase_form.html', context)
 
-#viewCase
+# viewCase
+
+
 def viewCase(request):
     current_user = request.user.id
     case_list = Case.objects.filter(created_by=current_user).order_by('-id')
-    page = request.GET.get('page',1)
-    paginator = Paginator(case_list,10)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(case_list, 10)
     try:
         case = paginator.page(page)
     except PageNotAnInteger:
@@ -302,10 +328,10 @@ def viewCase(request):
     except EmptyPage:
         case = paginator.page(paginator.num_pages)
     if request.method == 'GET':
-            search_query = request.GET.get('text_find', None)
-            if search_query:
-                case = Case.objects.filter(name__icontains=search_query,created_by=current_user)
-                context = {'case': case}
-                return render(request, 'cases/case.html', context)
+        search_query = request.GET.get('text_find', None)
+        if search_query:
+            case = Case.objects.filter(name__icontains=search_query, created_by=current_user)
+            context = {'case': case}
+            return render(request, 'cases/case.html', context)
     context = {'case': case}
     return render(request, 'cases/case.html', context)
