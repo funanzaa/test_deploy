645db1d112a2b9e16e9bc304231b1a3c689eb83c
funanzaa <eakachai_golf@hotmail.com>
2021-01-15 12:23:25 +0700

    created model server profile

--- mysite/crm/models.py
+++ mysite/crm/models.py
@@ -77,6 +77,43 @@ class ControlVersion(models.Model):
         return self.hcode + ' : ' + self.app_controlVersion
 
 
+class ServerBand(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class OperationSystem(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class ServerServiceStatus(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class ProfileServer(models.Model):
+    ServerBand = models.ForeignKey(ServerBand, null=True,on_delete= models.SET_NULL)
+    OperationSystem  = models.ForeignKey(OperationSystem, null=True,on_delete= models.SET_NULL)
+    ContactPhone = models.CharField(max_length=255,default='')
+    FixIpAddress = models.CharField(max_length=255,default='')
+    hospitals = models.ForeignKey(Hospitals, null=True, on_delete= models.SET_NULL)
+    datetimeSendServer = models.DateTimeField(null=True, blank=True)
+    case = models.ForeignKey(Case, null=True,on_delete= models.SET_NULL)
+    ServerServiceStatus = models.ForeignKey(ServerServiceStatus, null=True,on_delete= models.SET_NULL)
+    Memo = models.CharField(max_length=255,default='')
+    ContactFirstName = models.CharField(max_length=255,default='')
+    ContactLastName = models.CharField(max_length=255,default='')
+    ContactEmail = models.CharField(max_length=255,default='')
+    datetimeReceiveServer = models.DateTimeField(null=True, blank=True)
+    ServerImage = models.ImageField(blank=True)
+    update_at =  models.DateTimeField(null=True, blank=True)
+
+
+
+
+# model5
+
 class model5_lookup_error(models.Model):
     err_code = models.TextField()
     err_detail = models.TextField()
