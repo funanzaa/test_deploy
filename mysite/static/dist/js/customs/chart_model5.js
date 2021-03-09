07dca51bb9019e0432c1e6cf48fa1a751620bb7f
funanzaa <eakachai_golf@hotmail.com>
2021-03-09 10:15:17 +0700

    build sidebar apicases

--- mysite/static/dist/js/customs/chart_model5.js
+++ mysite/static/dist/js/customs/chart_model5.js
@@ -1,6 +1,6 @@
     $(document).ready(function(){
-            var endpoint = 'http://localhost:8000/api/model5HospSendClaimCode/data/'
-            // var endpoint = 'http://bkk.hospital-os.com/api/model5HospSendClaimCode/data/'
+            // var endpoint = 'http://localhost:8000/api/model5HospSendClaimCode/data/'
+            var endpoint = 'http://bkk.hospital-os.com/api/model5HospSendClaimCode/data/'
             var defaultData = []
             var labels = [];
             $.ajax({
